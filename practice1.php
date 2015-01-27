afdef3f9061361148fcae8c7a56e638ea51b3a8d
ei17ringo <ei17ringo@gmail.com>
2015-01-27 10:04:23 +0800

    add 3 files

---
+++ practice1.php
@@ -0,0 +1,15 @@
+<?php
+	$str = "hogehoge";
+	// $paramという変数に文字列を入れて変数を作って下さい
+    $param = "fugafuga";
+	// $param と $str を比較して、
+	// 同じ文字列であれば"同じです"と表示し、違う文字列であれば"違います"と表示する
+   if ($str === $param) {
+        echo "同じですね";
+        echo "\n";
+    } else {
+        echo "違いますね";
+        echo "\n";
+    }
+
+?>
---
+++ practice3.php
@@ -0,0 +1,17 @@
+<?php
+    // 連想配列を作成します
+    $array = array(
+        "first" => "hogehoge",
+        "second" => "fugafuga",
+        "third" => "mogemoge",
+        "forth" => "forforfor",
+        "fifth" => "hifive!"
+    );
+    // foreach文を使って、key, valueをそれぞれ毎回echoするような制御構文を書いて下さい
+    foreach ($array as $key => $val) {
+        echo $key;
+        echo ": ";
+        echo $val;
+        echo "<br>";
+    }
+?>
